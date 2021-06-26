50f41765f70716ebf7d1ec7ec1a0d1dc7b12cc18
JanFschr <JanFschr@users.noreply.github.com>
2021-06-26 22:02:39 +0200

    Update 0_rsync.sh

--- scripts/0_rsync.sh
+++ scripts/0_rsync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-!rsync -ah --exclude {"*_history"} --exclude {"*_autobackups"}  --info=progress2 "$1" "$2" ; echo Done
+rsync -ah --exclude {"*_history"} --exclude {"*_autobackups"}  --info=progress2 "$1" "$2" ; echo Done
