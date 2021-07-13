466d1acda2d5757d624de325061128f6d1b10e74
JanFschr <JanFschr@users.noreply.github.com>
2021-07-13 20:34:16 +0200

    Update 0_rsync.sh

--- scripts/0_rsync.sh
+++ scripts/0_rsync.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 date +"+++%R backup started+++"; rsync -ah --include {"*$3*"} --exclude {"*_history"} --exclude {"*_autobackups"}  --info=progress2 "$1" "$2" ; date +"+++%R backup finished+++";
-
